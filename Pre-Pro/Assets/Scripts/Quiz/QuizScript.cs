16c2bee3c3c86909c6f8ff4bd424e3c8ca8eb9ff
fmcfar200 <fmcfar200@caledonian.ac.uk>
2016-10-11 16:01:26 +0100

    Continued Proto
    Started Quiz Mechanic. Creates a random questions to display and
    provides answers. Still need to create a algorith that allows the player
    to clickk on the correct answer

---
+++ Pre-Pro/Assets/Scripts/Quiz/QuizScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class QuizScript : MonoBehaviour {
+
+    public Text questionObj;
+    public Button answer1;
+    public Button answer2;
+    public Button answer3;
+    public Button answer4;
+
+    public List<string> questions = new List<string>();
+
+    int questionNumber;
+    string correctAnswer;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        questionNumber = Random.Range(0, questions.Count);
+        questionObj.text = questions[questionNumber];
+
+        if (questionNumber == 0)
+        {
+            answer1.GetComponentInChildren<Text>().text = "4";
+            answer2.GetComponentInChildren<Text>().text = "2";
+            answer3.GetComponentInChildren<Text>().text = "8";
+            answer4.GetComponentInChildren<Text>().text = "6";
+            correctAnswer = "6";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+   
+}

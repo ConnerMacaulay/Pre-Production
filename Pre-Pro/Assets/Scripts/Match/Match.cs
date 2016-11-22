39703590c47654276797785d06594492ceaddea5
DiscoveryGold <CMACAU200@caledonian.ac.uk>
2016-11-22 11:08:50 +0000

    Match Game

---
+++ Pre-Pro/Assets/Scripts/Match/Match.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Match : MonoBehaviour {
+
+    public string selected = "null";    //stores the name of the selected OBJ
+    public string sComp = "null";   //stores the compareable string 
+    public int rnd; // stores the random number geenrated
+    public GameObject[] answer; //array of possible answers
+    public GameObject[] missing; // stores the possible missing tiles
+    public GameObject[] wrong; // stores the wrong objects that can be chosen
+   
+    // Use this for initialization
+    void Start ()
+    {
+       
+        rnd = Random.Range(0, 5); //sets rnd to a number between 0 and 5
+
+        missing[rnd].SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    { 
+        
+        // raycasts on mouse click to get the name of the object it collided with 
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                BoxCollider bc = hit.collider as BoxCollider;
+                if (bc != null)
+                {
+                    print(rnd);
+                    selected = bc.name;
+                }
+            }
+        }
+
+        if (selected != null)
+        {
+            GameObject selObj = GameObject.Find(selected);
+            print(selObj);
+            SpriteRenderer sr = selObj.GetComponent<SpriteRenderer>();
+            sr.color = Color.green;
+            sComp = selected.Substring(0, selected.Length - 1);
+            print("Selected: " + sComp + " Answer was: " + answer[rnd].name);
+        }
+
+        if (answer[rnd].name == selected)
+        {
+            print("correct");
+        }
+     }
+}
---
+++ Pre-Pro/Assets/Scripts/Match/TimerMatch.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerMatch : MonoBehaviour {
+
+    public float timeLeft;
+    public int timeLeftInt;
+    public Text timer;
+
+
+    void Start()
+    {
+       // timeLeft = 10; //Resetst the timer to the set time on start
+        TimerCount();
+    }
+    void Update()
+    {
+        //Ticks down the timer per second
+        if (timeLeft > 1)
+        {
+            timeLeft -= Time.deltaTime;
+            timeLeftInt = Mathf.FloorToInt(timeLeft);
+            TimerCount();
+           // Debug.Log(timeLeftInt);
+        }
+
+        if (timeLeftInt == 0)
+        {
+            StartCoroutine(WaitAndLoadLevel(1.0f));
+        }
+    }
+    //Converts the Int to a string and sends it to the timer Obj
+    void TimerCount ()
+    {
+        timer.text = timeLeftInt.ToString();
+    }
+
+    public void AddTime(float time)
+    {
+        timeLeft += time;
+    }
+
+    IEnumerator WaitAndLoadLevel(float t)
+    {
+        Cursor.visible = false;
+        yield return new WaitForSeconds(t);
+        Application.LoadLevel("Quiz");
+
+
+    }
+}

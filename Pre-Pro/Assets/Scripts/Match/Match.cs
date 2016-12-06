35463169468ea296900b6b3e8cc1ca796ad1c6ac
DiscoveryGold <CMACAU200@caledonian.ac.uk>
2016-12-06 15:47:04 +0000

    Tile game
    -Added transitions to quiz
    -Added diffrent colours for right and wrong choices
    -Altered timer to allow for remembering the tiles
    -Dont show the answers until the tile is missing

--- Pre-Pro/Assets/Scripts/Match/Match.cs
+++ Pre-Pro/Assets/Scripts/Match/Match.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Match : MonoBehaviour {
+
+    public float timeLeft;
+    public int timeLeftInt;
+    public Text timer;
+    public bool hide = false; // checks if the hidden tile is missing 
 
     public string selected = "null";    //stores the name of the selected OBJ
     public string sComp = "null";   //stores the compareable string 
@@ -9,21 +15,46 @@ public class Match : MonoBehaviour {
     public GameObject[] answer; //array of possible answers
     public GameObject[] missing; // stores the possible missing tiles
     public GameObject[] wrong; // stores the wrong objects that can be chosen
-   
+    public GameObject choice;
+
     // Use this for initialization
     void Start ()
     {
-       
+        // timeLeft = 10; //Resetst the timer to the set time on start
+        TimerCount();
+
         rnd = Random.Range(0, 5); //sets rnd to a number between 0 and 5
 
-        missing[rnd].SetActive(false);
+        //choice = GameObject.Find("Choice");
 
     }
 	
 	// Update is called once per frame
 	void Update ()
-    { 
-        
+    {
+        //Ticks down the timer per second
+        if (timeLeft > 1)
+        {
+            timeLeft -= Time.deltaTime;
+            timeLeftInt = Mathf.FloorToInt(timeLeft);
+            TimerCount();
+            // Debug.Log(timeLeftInt);
+        }
+
+        if (timeLeftInt == 0 && hide == false)
+        {
+            timeLeft = 10;
+            hide = true;
+            missing[rnd].SetActive(false);
+            choice.SetActive (true);
+            return;
+        }
+
+        if (timeLeftInt == 0 && hide == true)
+        {
+            StartCoroutine(WaitAndLoadLevel(1.0f));
+        }
+
         // raycasts on mouse click to get the name of the object it collided with 
         if (Input.GetMouseButtonDown(0))
         {
@@ -43,17 +74,38 @@ public class Match : MonoBehaviour {
 
         if (selected != null)
         {
-            GameObject selObj = GameObject.Find(selected);
-            print(selObj);
+            GameObject selObj = GameObject.Find(selected); 
             SpriteRenderer sr = selObj.GetComponent<SpriteRenderer>();
-            sr.color = Color.green;
             sComp = selected.Substring(0, selected.Length - 1);
             print("Selected: " + sComp + " Answer was: " + answer[rnd].name);
+            if (answer[rnd].name == selected)
+            {
+                sr.color = Color.green;
+                print("Correct Tile Selected!");
+                StartCoroutine(WaitAndLoadLevel(1.0f));
+            }
+            else
+            {
+                sr.color = Color.red;
+            }
         }
+    }
 
-        if (answer[rnd].name == selected)
-        {
-            print("correct");
-        }
-     }
+    //Converts the Int to a string and sends it to the timer Obj
+    void TimerCount()
+    {
+        timer.text = timeLeftInt.ToString();
+    }
+
+    public void AddTime(float time)
+    {
+        timeLeft += time;
+    }
+
+    IEnumerator WaitAndLoadLevel(float t)
+    {
+        Cursor.visible = false;
+        yield return new WaitForSeconds(t);
+        Application.LoadLevel("Quiz");
+    }
 }
